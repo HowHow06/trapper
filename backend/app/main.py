f7697f3ad009be73a9322383e5aaf6a16f93ead4
Howard <howard_bb@hotmail.com>
2023-06-12 22:55:17 +0800

    feat: fix .env file path

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-# from core import settings
+from .core import settings
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "Worldo"}
+
+
+@app.get("/health-check")
+def read_root():
+    return {
+        "db": settings.mysql_database
+    }
