6d14f187e3a04e4dc2b88cb31de69eb0c869c419
Howard <howard_bb@hotmail.com>
2023-06-13 00:04:54 +0800

    feat: add user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+import datetime
+
+from sqlmodel import SQLModel, Field
+from passlib.hash import bcrypt
+from pydantic import EmailStr
+
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    hashed_password: str
+    email: EmailStr
+    created_at: datetime.datetime = datetime.datetime.now()
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.hashed_password)
