3d80d6d2c0a9926f2f56f83f0a635cbac1b77207
Howard <howard_bb@hotmail.com>
2023-06-13 19:06:40 +0800

    feat: only import testing route when is dev environment

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,11 +1,16 @@
 from app.api.api_v1.endpoints import auth, healthcheck
-from app.api.api_v1.endpoints.secured import testSecured
+from app.api.api_v1.endpoints.secured import test_secured
+from app.core.config import settings
 from fastapi import APIRouter
 
 api_router = APIRouter()
+# tags are for openapi document
 api_router.include_router(auth.router, tags=["auth"])
-api_router.include_router(healthcheck.router, tags=["healthcheck"])
-api_router.include_router(testSecured.router, tags=["testSecured"])
+
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
 # api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
+
+if settings.ENVIRONMENT == "dev":
+    api_router.include_router(healthcheck.router, tags=["healthcheck"])
+    api_router.include_router(test_secured.router, tags=["test-secured"])
