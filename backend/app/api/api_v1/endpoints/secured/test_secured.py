089596aff2d38dddf689f87809e09548a4fa7f8b
Howard <howard_bb@hotmail.com>
2023-06-13 19:06:14 +0800

    feat: add more testing routes

---
+++ backend/app/api/api_v1/endpoints/secured/test_secured.py
@@ -0,0 +1,19 @@
+from app import schemas
+from app.api import deps
+# from core.auth import verify_password, create_access_token
+from app.models import User as UserModel
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+router = APIRouter()
+
+
+@router.post("/test-secured-user", response_model=schemas.User)
+async def test_secured_active_user(current_user: UserModel = Depends(deps.get_current_active_user)):
+    # FastAPI will wait for the get_current_user dependency funciton by default, no need to add await keyword
+    return current_user
+
+
+@router.post("/test-secured-admin", response_model=schemas.User)
+async def test_secured_active_admin(current_admin: UserModel = Depends(deps.get_current_active_admin)):
+    return current_admin
