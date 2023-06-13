0e5e4a607b4aa1ccd3de1c28a6def8c682b54af5
Howard <howard_bb@hotmail.com>
2023-06-13 18:00:36 +0800

    feat: add schemas for user

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -3,6 +3,11 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
+class UserAuth(BaseModel):
+    username: str
+    password: str
+
+
 # Shared properties
 class UserBase(BaseModel):
     username: str
