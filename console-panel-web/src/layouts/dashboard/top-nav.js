bf88ec6b68173c77837b6dd03fca21fbb209d895
Howard <howard_bb@hotmail.com>
2023-06-14 16:56:55 +0800

    feat: use info from token

--- console-panel-web/src/layouts/dashboard/top-nav.js
+++ console-panel-web/src/layouts/dashboard/top-nav.js
@@ -16,6 +16,8 @@ import {
 import { alpha } from '@mui/material/styles';
 import { usePopover } from 'src/hooks/use-popover';
 import { AccountPopover } from './account-popover';
+import { useAuth } from 'src/hooks/use-auth';
+import { DEFAULT } from 'src/constants/variables';
 
 const SIDE_NAV_WIDTH = 280;
 const TOP_NAV_HEIGHT = 64;
@@ -24,6 +26,7 @@ export const TopNav = (props) => {
   const { onNavOpen } = props;
   const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
   const accountPopover = usePopover();
+  const auth = useAuth();
 
   return (
     <>
@@ -94,7 +97,7 @@ export const TopNav = (props) => {
                 height: 40,
                 width: 40,
               }}
-              src="/assets/avatars/avatar-anika-visser.png"
+              src={auth.user?.avatar || DEFAULT.AVATAR}
             />
           </Stack>
         </Stack>
