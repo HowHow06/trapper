a2ed41aa7c1ba73788d0e12e1d285e993352c133
Howard <howard_bb@hotmail.com>
2023-06-15 23:50:00 +0800

    feat: ready for react DOM content script

--- extension-vite/src/content/index.jsx
+++ extension-vite/src/content/index.jsx
@@ -12,4 +12,6 @@ ReactDOM.createRoot(appContainer).render(
     <div>Hello Im ContentScipt DOM</div>
   </React.StrictMode>,
 )
+
 // cannot import component from other file, else will show error Error: @vitejs/plugin-react can't detect preamble. Something is wrong. See https://github.com/vitejs/vite-plugin-react/pull/11#discussion_r430879201
+// if want to use this jsx file, change in manifest.js
