20bb29c809844f7abfc03fb46279629a69dde292
Howard <howard_bb@hotmail.com>
2023-06-15 19:18:15 +0800

    refactor: sort import

--- extension-vite/src/options/index.jsx
+++ extension-vite/src/options/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Options'
 import '../theme.scss'
+import App from './Options'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
