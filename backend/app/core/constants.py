04fe5882b2fa64dc6a6b7488abf37b280d9eb686
Howard <howard_bb@hotmail.com>
2023-06-27 17:40:22 +0800

    feat: add constants

---
+++ backend/app/core/constants.py
@@ -0,0 +1,15 @@
+class Status(object):
+    WAITING = 1
+    RUNNING = 2
+    DONE = 3
+    KILLED = 4
+
+
+class Severity(object):
+    LOW = 5
+    MEDIUM = 6
+    HIGH = 7
+
+
+class VulnerabilityType(object):
+    XSS = 8
