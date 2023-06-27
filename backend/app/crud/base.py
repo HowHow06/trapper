05a2cb2bd9623899c070955eb731d13eaa1af51a
Howard <howard_bb@hotmail.com>
2023-06-27 17:30:11 +0800

    feat: modify base crud to adapt to soft delete

--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -1,6 +1,8 @@
-from typing import Generic, List, Optional, Type, TypeVar
+from datetime import datetime
+from typing import Dict, Generic, List, Optional, Type, TypeVar
 
-from sqlalchemy import delete, select, update
+from sqlalchemy import and_, delete, select
+from sqlalchemy import update as sql_update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import SQLModel
 
@@ -22,11 +24,23 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self.model = model
 
     async def get(self, db: AsyncSession, id: int) -> Optional[ModelType]:
-        result = await db.execute(select(self.model).filter(self.model.id == id))
+        statement = select(self.model).filter(
+            and_(self.model.id == id, self.model.deleted_at.is_(None)))
+        result = await db.execute(statement)
         return result.scalars().first()
 
-    async def get_multi(self, db: AsyncSession, skip: int = 0, limit: int = 100) -> List[ModelType]:
-        result = await db.execute(select(self.model).offset(skip).limit(limit))
+    # Can add where parameter to this function:
+    # from sqlalchemy import or_
+    # where_clause = or_(Task.status_id == 1, Task.status_id == 2)
+    # result = await crud.get_multi(db, where=where_clause)
+    async def get_multi(self, db: AsyncSession, skip: int = 0, limit: int = 100, where=None) -> List[ModelType]:
+        conditions = [self.model.deleted_at.is_(None)]
+
+        if where is not None:
+            conditions.append(where)
+        statement = select(self.model).filter(
+            and_(*conditions)).offset(skip).limit(limit)
+        result = await db.execute(statement)
         return result.scalars().all()
 
     # whatever parameter appear after the * must be explicitly specified when using
@@ -38,6 +52,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return db_obj
 
     async def update(self, db: AsyncSession, *, db_obj: ModelType, obj_in: UpdateSchemaType) -> ModelType:
+        if db_obj.deleted_at is not None:
+            return db_obj
+
         obj_data = db_obj.dict()
         update_data = obj_in.dict(exclude_unset=True)
         for field in obj_data:
@@ -47,8 +64,31 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         await db.refresh(db_obj)
         return db_obj
 
+    # Sample usage of this function
+    # values = {"status_id": 2, "completed_at": datetime.utcnow()}
+    # where_clause = Task.status_id == 1
+    # updated_rows = await crud.update_by_where(db, values, where=where_clause)
+    # print(f"Updated {updated_rows} rows.")
+    async def update_by_where(self, db: AsyncSession, values: Dict[str, any], where=None) -> int:
+        if not values:
+            return 0
+
+        conditions = [self.model.deleted_at.is_(None)]
+        if where is not None:
+            conditions.append(where)
+
+        stmt = sql_update(self.model).where(and_(*conditions)).values(values)
+        result = await db.execute(stmt)
+        await db.commit()
+
+        return result.rowcount
+
     async def remove(self, db: AsyncSession, *, id: int) -> ModelType:
         obj = await self.get(db, id=id)
-        await db.execute(delete(self.model).where(self.model.id == id))
-        await db.commit()
+        # await db.execute(delete(self.model).where(self.model.id == id))
+        # await db.commit()
+        if obj:
+            obj.deleted_at = datetime.utcnow()
+            await db.commit()
+            await db.refresh(obj)
         return obj
