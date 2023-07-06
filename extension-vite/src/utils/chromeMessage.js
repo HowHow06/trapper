b5f71e3f5bab6c221be07994f25948f8e0285f93
Howard <howard_bb@hotmail.com>
2023-07-07 00:16:35 +0800

    feat: helper function to send runtime message

--- extension-vite/src/utils/chromeMessage.js
+++ extension-vite/src/utils/chromeMessage.js
@@ -1,27 +1,26 @@
-/**
- * will trigger the onMessage
- * @param message
- * @param callback
- */
-export function sendCurrentTabMessagePromise(message) {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tabId = tabs[0].id
-      chrome.tabs.sendMessage(tabId, message, (response) => {
-        if (response.complete) {
-          resolve()
-        } else {
-          reject('Something wrong')
-        }
-      })
-    })
-  })
-}
+// /**
+//  * will trigger the onMessage
+//  * @param message
+//  * @param callback
+//  */
+// export function sendCurrentTabMessagePromise(message) {
+//   return new Promise((resolve, reject) => {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       const tabId = tabs[0].id
+//       chrome.tabs.sendMessage(tabId, message, (response) => {
+//         if (response.complete) {
+//           resolve()
+//         } else {
+//           reject('Something wrong')
+//         }
+//       })
+//     })
+//   })
+// }
 
 /**
- *  发送消息给background
+ *  will trigger background actions
  * @param message
- * @param callback
  */
 export function sendRuntimeMessagePromise(message) {
   return new Promise((resolve, reject) => {
