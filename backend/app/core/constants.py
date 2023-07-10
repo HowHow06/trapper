9c7aedc899410e5d306f3fdf1ef7d10eefb57949
Howard <howard_bb@hotmail.com>
2023-07-10 13:54:47 +0800

    feat: add more constants

--- backend/app/core/constants.py
+++ backend/app/core/constants.py
@@ -1,3 +1,17 @@
+
+# Default POST data content-type
+DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded"
+
+# Raw text POST data content-type
+PLAIN_TEXT_CONTENT_TYPE = "text/plain"
+
+# json text POST data content-type
+JSON_TEXT_CONTENT_TYPE = "application/json"
+
+# form text POST data content-type
+FORM_DATA_CONTENT_TYPE = "multipart/form-data"
+
+
 # Just a temporary solution, by right should get the id from database
 class Status(object):
     WAITING = 1
@@ -14,3 +28,9 @@ class Severity(object):
 
 class VulnerabilityType(object):
     XSS = 8
+
+
+class Vulnerability(object):
+    REFLECTED_XSS = 1
+    STORED_XSS = 2
+    DOM_XSS = 3
