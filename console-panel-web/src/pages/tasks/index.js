19eb5e396fabc389c66176ab375cb0716c6217f4
Howard <howard_bb@hotmail.com>
2023-07-10 22:47:54 +0800

    feat: tasks page

---
+++ console-panel-web/src/pages/tasks/index.js
@@ -0,0 +1,100 @@
+import { Box, Container, Stack, Typography } from '@mui/material';
+import Head from 'next/head';
+import { useEffect, useRef, useState } from 'react';
+import api from 'src/api';
+import CommonTable from 'src/components/common/common-table';
+import { DashboardLayout } from 'src/layouts/dashboard/dashboard-layout';
+import { isApiSuccess } from 'src/utils/api-call';
+
+const columns = [
+  {
+    name: 'Task Name',
+    accessor: 'task_name',
+  },
+  {
+    name: 'URL Rule',
+    accessor: 'url_rule',
+  },
+  {
+    name: 'Status',
+    accessor: 'task_status_id',
+    type: 'task_status',
+  },
+  {
+    name: 'Created At',
+    accessor: 'created_at',
+    type: 'datetime',
+  },
+  {
+    name: 'Stopped At',
+    accessor: 'stopped_at',
+    type: 'datetime',
+  },
+];
+
+const Page = () => {
+  const ignore = useRef(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [shouldRefresh, setShouldRefresh] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const initialize = async () => {
+      const fetchTasks = async () => {
+        const response = await api.getTasks({ sortBy: 'created_at', isDescOrder: true });
+        if (response && isApiSuccess(response)) {
+          const tasks = response.data;
+          setTasks(tasks);
+        }
+      };
+
+      if (ignore.current) {
+        return;
+      }
+      ignore.current = true;
+
+      await Promise.all([fetchTasks()]);
+
+      setIsLoading(false);
+    };
+
+    initialize();
+  }, [shouldRefresh]);
+
+  return (
+    <>
+      <Head>
+        <title>Tasks | Trapper Console</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">Tasks</Typography>
+              </Stack>
+            </Stack>
+            {/* <CustomersSearch /> */}
+            <CommonTable
+              columns={columns}
+              data={tasks}
+              itemHref={`/tasks`}
+              identity="id"
+              isLoading={isLoading}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
--- console-panel-web/src/sections/overview/overview-recent-tasks.js
+++ console-panel-web/src/sections/overview/overview-recent-tasks.js
@@ -1,31 +1,34 @@
 import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
 import { Box, Button, Card, CardActions, CardHeader, Divider, SvgIcon } from '@mui/material';
+import { useRouter } from 'next/navigation';
 import PropTypes from 'prop-types';
 import { Scrollbar } from 'src/components/scrollbar';
 import CommonTable from '../../components/common/common-table';
 
+const columns = [
+  {
+    name: 'Task Name',
+    accessor: 'task_name',
+  },
+  {
+    name: 'URL Rule',
+    accessor: 'url_rule',
+  },
+  {
+    name: 'Status',
+    accessor: 'task_status_id',
+    type: 'task_status',
+  },
+  {
+    name: 'Created At',
+    accessor: 'created_at',
+    type: 'datetime',
+  },
+];
+
 const OverviewRecentTasks = (props) => {
   const { tasks = [], sx, isLoading = false } = props;
-  const columns = [
-    {
-      name: 'Task Name',
-      accessor: 'task_name',
-    },
-    {
-      name: 'URL Rule',
-      accessor: 'url_rule',
-    },
-    {
-      name: 'Status',
-      accessor: 'task_status_id',
-      type: 'task_status',
-    },
-    {
-      name: 'Created At',
-      accessor: 'created_at',
-      type: 'datetime',
-    },
-  ];
+  const router = useRouter();
 
   return (
     <Card sx={sx}>
@@ -35,7 +38,7 @@ const OverviewRecentTasks = (props) => {
           <CommonTable
             columns={columns}
             data={tasks.slice(0, 10)}
-            //   itemHref={`/communities/${community.uuid}/participants`}
+            itemHref={`/tasks`}
             identity="id"
             noPagination
             isLoading={isLoading}
@@ -54,6 +57,9 @@ const OverviewRecentTasks = (props) => {
           size="small"
           variant="text"
           // TODO: navigate to tasks
+          onClick={() => {
+            router.push('/tasks');
+          }}
         >
           View all
         </Button>
